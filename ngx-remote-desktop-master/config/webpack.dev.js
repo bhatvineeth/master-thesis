26a1f6c59ed0a0658115f61882091d760e42445b
bhatvineeth <59833585+bhatvineeth@users.noreply.github.com>
2021-08-19 16:29:11 +0200

    configured docker

--- ngx-remote-desktop-master/config/webpack.dev.js
+++ ngx-remote-desktop-master/config/webpack.dev.js
@@ -17,6 +17,11 @@ module.exports = function (options) {
     }), {
         devtool: 'cheap-module-source-map',
         devServer: {
+						host: '0.0.0.0',
+						disableHostCheck: true,
+						watchOptions: {
+							poll: true
+						},
             port: 9999,
             hot: options.HMR,
             stats: {
